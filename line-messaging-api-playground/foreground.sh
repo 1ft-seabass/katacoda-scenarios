2d51c36452c6af6f7e2d0f12695ae5e4594d8320
tseigo <tnk.seigo+bitbucket@gmail.com>
2020-02-10 15:41:16 +0900

    Adding to n commnd for updating node.js v12

--- line-messaging-api-playground/foreground.sh
+++ line-messaging-api-playground/foreground.sh
@@ -1,5 +1,8 @@
 echo "This is automatically run when the scenario"
 
+sudo npm install -g n
+sudo n 12.15.0
+
 git clone https://github.com/1ft-seabass/katacoda-line-messaging-api-playground.git example
 
 cd example
